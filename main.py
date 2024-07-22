cb3553010941a5d8617781cf81a3da9175e125b7
Odemi Tosanwumi <46735063+odemiit@users.noreply.github.com>
2024-07-22 22:24:13 +0000

    import the logo and print it at the start of the game

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-#A number guessing game where the user has to guess a number between 1 and 100
+# A number guessing game where the user has to guess a number between 1 and 100
+
+from art import logo
+
+print(logo)
